112afae1ec2e79db32db63811ff26624211acc39
raulsandes <69020448+raulsandes@users.noreply.github.com>
2021-02-06 15:58:24 -0300

    Add files via upload

---
+++ MoveZeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int last = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0){
+                nums[last] = nums[i];
+                last++;
+            }
+        }
+        for(int i=last; i<n; i++){
+            nums[i]=0;
+        }
+    }
+};
